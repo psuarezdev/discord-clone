d5ff43ca066ca0770a2452e655ae2548d259ff0d
Pablo Suárez <104940521+psuarezdev@users.noreply.github.com>
2024-02-12 09:35:15 +0000

    Delete prisma/migrations directory

--- prisma/migrations/20240211150240_adding_public_id_to_prisma_messages/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "direct_messages" ADD COLUMN     "file_id" TEXT;
-
--- AlterTable
-ALTER TABLE "messages" ADD COLUMN     "file_id" TEXT;
--- prisma/migrations/20240211161032_making_message_content_nullable/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "direct_messages" ALTER COLUMN "content" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "messages" ALTER COLUMN "content" DROP NOT NULL;
